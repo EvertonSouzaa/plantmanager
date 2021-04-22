813f08bb0be51b993cfd2c8bb9038a01ffa5faca
VetControl <everton@vetcontrolonline.com.br>
2021-04-22 19:48:06 -0300

    tela começar pronta

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { TouchableOpacity, StyleSheet, TouchableOpacityProps, Text } from "react-native";
-import { MaterialIcons } from '@expo/vector-icons';
 import colors from "../styles/colors"
+import fonts from "../styles/fonts"
 
 
 interface ButtonProps extends TouchableOpacityProps {
@@ -11,24 +11,28 @@ interface ButtonProps extends TouchableOpacityProps {
 
 export function Button({ title, ...rest }: ButtonProps) {
   return (
-    <TouchableOpacity style={styles.btn} activeOpacity={0.5} {...rest}>
+    <TouchableOpacity
+      style={styles.container}
+      activeOpacity={0.5}
+      {...rest}
+    >
       <Text style={styles.btnText}> {title} </Text>
     </TouchableOpacity>
   )
 }
 
 const styles = StyleSheet.create({
-  btn: {
+  container: {
     backgroundColor: colors.green,
     justifyContent: "center",
     alignItems: "center",
     borderRadius: 16,
     marginBottom: 10,
     height: 56,
-    width: 56
   },
   btnText: {
     color: colors.white,
-    fontSize: 24
+    fontSize: 24,
+    fontFamily: fonts.heading
   }
 })
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,24 +1,48 @@
 import React from "react";
-import { SafeAreaView, View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import {
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Dimensions,
+  View
+} from "react-native";
 import wateringImg from "../img/watering.png";
 import colors from "../styles/colors";
-import { Button } from "../components/Button"
+import fonts from "../styles/fonts";
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from "@react-navigation/core"
 
 export function Welcome() {
+  const navigation = useNavigation()
+
+  function handleStart() {
+    navigation.navigate("UserIdentification")
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>
-        Gerencie {"\n"}
-        suas plantas {"\n"}
-        de forma fácil
-      </Text>
-      <Image source={wateringImg} style={styles.image} />
-      <Text style={styles.subtitle}>
-        Não esqueça mais de regar suas plantas.
-        Nós cuidamos de lembrar você sempre que precisar.
-      </Text>
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Gerencie {"\n"}
+          suas plantas de {"\n"}
+          forma fácil
+        </Text>
+        <Image
+          source={wateringImg}
+          style={styles.image}
+          resizeMode="contain"
+        />
+        <Text style={styles.subtitle}>
+          Não esqueça mais de regar suas plantas.
+          Nós cuidamos de lembrar você sempre que precisar.
+        </Text>
 
-      <Button title=">" onPress={() => { }} />
+        <TouchableOpacity style={styles.btn} onPress={handleStart}>
+          <Feather name="chevron-right" size={32} color={colors.white} />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   )
 }
@@ -26,26 +50,42 @@ export function Welcome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+
+
+  },
+  wrapper: {
+    flex: 1,
     alignItems: "center",
-    justifyContent: "space-between"
+    justifyContent: "space-around",
+    paddingHorizontal: 20
   },
   title: {
-    fontSize: 32,
-    fontWeight: "bold",
+    fontSize: 28,
     textAlign: "center",
     color: colors.heading,
-    marginTop: 38
+    marginTop: 38,
+    fontFamily: fonts.heading,
+    lineHeight: 34
   },
   subtitle: {
     textAlign: "center",
     fontSize: 18,
     paddingHorizontal: 20,
-    color: colors.heading
+    color: colors.heading,
+    fontFamily: fonts.text
   },
 
   image: {
-    width: 292,
-    height: 284
+    height: Dimensions.get("window").width * 0.7
+  },
+  btn: {
+    backgroundColor: colors.green,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56
   },
 
 })
